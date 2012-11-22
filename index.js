6b7dcbf261e7e4fd87a77bec5681094ef2a5fdef
John Wright <johngeorge.wright@gmail.com>
2012-11-22 15:41:19 +1100

    Can now load a module index without having an actual file. Fixes #3

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var fs   = require('fs'),
-    path = require('path');
+    path = require('path'),
+    Mod  = require('module');
 
-module.exports = function(mod){
-    var dirname = path.dirname(mod.filename),
-        dir     = fs.readdirSync(dirname),
-        i, file, moduleName;
+function loadFromModule(mod){
+  var dirname = path.dirname(mod.filename),
+      dir     = fs.readdirSync(dirname),
+      i, file, moduleName;
 
   // Loop through all the files in this directory
   for(i=0; i<dir.length; i++){
@@ -17,5 +18,31 @@ module.exports = function(mod){
       mod.exports[moduleName] = require(path.join(dirname, moduleName));
     }
   }
+}
+
+function loadFromName(mod){
+  var parent   = module.parent,
+      dirname  = path.dirname(parent.filename),
+      filename = path.join(dirname, mod, 'index') + '.js';
+
+  mod          = new Mod(filename);
+  mod.filename = mod.id;
+  mod.parent   = parent;
+
+  loadFromModule(mod);
+
+  mod.loaded = true;
+
+  return mod;
+}
+
+module.exports = function(mod){
+  if(mod.filename){
+    loadFromModule(mod);
+  }else{
+    mod = loadFromName(mod);
+  }
+
+  return mod.exports;
 };
 
--- test/module-index.js
+++ test/module-index.js
@@ -2,21 +2,40 @@ var expect      = require('expect.js'),
     path        = require('path'),
     moduleIndex = require('../index.js');
 
-describe('module-index()', function(){
+describe('module-index', function(){
 
-  beforeEach(function(){
-    this.mod = {
-      filename : path.join(__dirname, 'test-module/index.js'),
-      exports  : {}
-    };
-    moduleIndex(this.mod);
+  describe('within it\'s own file', function(){
+
+    beforeEach(function(){
+      this.mod = {
+        filename : path.join(__dirname, 'test-module/index.js'),
+        exports  : {}
+      };
+      moduleIndex(this.mod);
+    });
+
+    it('should include all of it\'s sibling modules', function(){
+      expect(this.mod.exports).to.be.an(Object);
+      expect(this.mod.exports.mung).not.to.be(undefined);
+      expect(this.mod.exports.face).not.to.be(undefined);
+      expect(this.mod.exports.mungface).not.to.be(undefined);
+    });
+
   });
 
-  it('should include all of it\'s sibling modules', function(){
-    expect(this.mod.exports).to.be.an(Object);
-    expect(this.mod.exports.mung).not.to.be(undefined);
-    expect(this.mod.exports.face).not.to.be(undefined);
-    expect(this.mod.exports.mungface).not.to.be(undefined);
+  describe('without an index file', function(){
+
+    beforeEach(function(){
+      this.mod = moduleIndex('./test-module');
+    });
+
+    it('should include all of it\'s sibling modules', function(){
+      expect(this.mod).to.be.an(Object);
+      expect(this.mod.mung).not.to.be(undefined);
+      expect(this.mod.face).not.to.be(undefined);
+      expect(this.mod.mungface).not.to.be(undefined);
+    });
+
   });
 
 });
