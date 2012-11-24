0a8d8286594f510019f56803e77d862ab48f834c
John Wright <johngeorge.wright@gmail.com>
2012-11-24 19:51:12 +1100

    Wrong condition at file-type check.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = function(mod){
   for(i=0; i<dir.length; i++){
     file = dir[i];
     // Unless the file is hidden or is this file
-    if(file[0] !== '.' || file !== mod.filename){
+    if(file[0] !== '.' && file !== mod.filename){
       // Find the file's module name by removing it's extension
       moduleName = path.basename(file, path.extname(file));
       // Require the module and add it to the exports object
